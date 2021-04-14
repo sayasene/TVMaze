8e093381b111bfb293b610a3dffa6de9f906f752
SAYASENE <MITCHELLSAYASENE@GMAIL.COM>
2021-04-14 15:41:14 -0400

    working on mapping of show JSON to parse only keys we need

--- tvmaze.js
+++ tvmaze.js
@@ -12,27 +12,27 @@ const $searchForm = $("#searchForm");
  *    (if no image URL given by API, put in a default image URL)
  */
 
-async function getShowsByTerm( /* term */) {
-  // ADD: Remove placeholder & make request to TVMaze search shows API.
+ function pick (obj, props) {
 
-  return [
-    {
-      id: 1767,
-      name: "The Bletchley Circle",
-      summary:
-        `<p><b>The Bletchley Circle</b> follows the journey of four ordinary 
-           women with extraordinary skills that helped to end World War II.</p>
-         <p>Set in 1952, Susan, Millie, Lucy and Jean have returned to their 
-           normal lives, modestly setting aside the part they played in 
-           producing crucial intelligence, which helped the Allies to victory 
-           and shortened the war. When Susan discovers a hidden code behind an
-           unsolved murder she is met by skepticism from the police. She 
-           quickly realises she can only begin to crack the murders and bring
-           the culprit to justice with her former friends.</p>`,
-      image:
-          "http://static.tvmaze.com/uploads/images/medium_portrait/147/369403.jpg"
-    }
-  ]
+	// Create new object
+	var picked = {};
+
+	// Loop through props and push to new object
+	for (let prop of props) {
+		picked[prop] = obj[prop];
+	}
+
+	// Return new object
+	return picked;
+
+}
+
+async function getShowsByTerm(term) {
+  // ADD: Remove placeholder & make request to TVMaze search shows API.
+   let response = await axios.get('http://api.tvmaze.com/search/shows',{params:{q:term}})
+  //  map
+  console.log(response.data.map(pick(response.data,['show.id','show.name','show.summary','show.image.medium'])))
+    return response.data.map(pick(response.data,['show.id','show.name','show.summary','show.image.medium']));
 }
 
 
